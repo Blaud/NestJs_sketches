b4f309a6dc8dd9354908315c29d3638164e38d4c
blaud <iblaudi@gmail.com>
2018-10-16 02:32:00 +0700

    Heroku deploy7 (port fix)

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: process.env.MONGODB_URI || "mongodb://127.0.0.1/mydb",
+    jwt: process.env.JWT || "dev jwt",
+    port: process.env.PORT || 3000
+};
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as keys from '../config/keys';
+
+const port = keys.port;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(port, () => console.log(`Server started on ${port}`));
 }
 bootstrap();
